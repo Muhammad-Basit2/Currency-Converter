95f727de15810bb65bdb7dbd0c995a6cea9b92f5
Muhammad-Basit2 <muhammadbasit9274@gmail.com>
2026-05-09 00:59:37 +0500

    add functions

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.min.json";
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
 
 
 for (let select of dropdowns) {
@@ -30,3 +33,18 @@ const updateFlag = (element) => {
     let img = element.parentElement.querySelector("img");
     img.src = newSrc;
 };
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amVal = amount.value;
+    if (amVal === "" || amVal < 1) {
+        amVal = 1;
+        amount.value = "1";
+    }
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    console.log(rate);
+});
